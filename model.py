21c697be9321cd25572d9a492ee322bc89acc04e
SolderingIronUser <wasiliysitnikov@gmail.com>
2021-10-23 00:41:29 +0300

    Update model.py

--- model.py
+++ model.py
@@ -10,14 +10,13 @@ def suum():
     except:
         view.messagebox.showerror("Error","AMOGUS, кто то ввел не число")
        
-def plotting():
-    x=[0,3,1.5,0,3,4.5,4.5,3.5,4.5,5.5,4.5,4.5,6,6,6,8,8]
-    y=[0,6,3,6,0,0,3,6,3,6,3,0,0,6,0,6,0]
-    def do_plot(x, y):
-        try:
-            [ax[x].clear() for x in range(1)]
-            ax[0].plot(x,y)
-            view.canvas.draw()
-            view.messagebox.askokcancel("Нет ошибки, график отрисован?")
-        except:
-            view.messagebox.showwarning("Где-то ошибка")
+x=[0,3,1.5,0,3,4.5,4.5,3.5,4.5,5.5,4.5,4.5,6,6,6,8,8]
+y=[0,6,3,6,0,0,3,6,3,6,3,0,0,6,0,6,0]
+def do_plot():
+    try:
+        #[ax[x].clear() for x in range(1)]
+        #ax[0].plot(x,y)
+        #view.canvas.draw()
+        view.messagebox.askokcancel("Нет ошибки, график отрисован?")
+    except:
+        view.messagebox.showwarning("Где-то ошибка")
