fb156578b66aea43eb8562e4cb30e6071e6df630
SolderingIronUser <wasiliysitnikov@gmail.com>
2021-10-23 00:57:37 +0300

    Update model.py

--- model.py
+++ model.py
@@ -15,9 +15,9 @@ def do_plot():
     x=[0,3,1.5,0,3,4.5,4.5,3.5,4.5,5.5,4.5,4.5,6,6,6,8,8]
     y=[0,6,3,6,0,0,3,6,3,6,3,0,0,6,0,6,0]
     try:
-        #[ax[x].clear() for x in range(1)]
-        #ax[0].plot(x,y)
-        #view.canvas.draw()
-        view.messagebox.askokcancel("Нет ошибки, график отрисован?")
+        [ax[x].clear() for x in range(1)]
+        ax[0].plot(x,y)
+        view.canvas.draw()
+        view.messagebox.askokcancel("Привет!","Нет ошибки, график отрисован?")
     except:
         view.messagebox.showwarning("Где-то ошибка")
