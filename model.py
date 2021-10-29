c0f33e9ffe654d678f6b54477f3ea77b8ca21bcc
SolderingIronUser <wasiliysitnikov@gmail.com>
2021-10-29 08:44:01 +0300

    Update model.py

--- model.py
+++ model.py
@@ -29,7 +29,7 @@ def do_plot(need_city, points):
     #print(x)
     #print(y)
     #[view.ax[x].clear() for x in range(1)]
-    view.ax[0].fill_between(x,y,label="Q="+str(need_city/1000))
+    view.ax[0].fill_between(x,y,label="Q="+str(need_city/1000)+"МВт*ч/год")
     view.figure.legend(loc = "upper left")
     view.canvas.draw()
         # view.message_ask(["Request!", "Нет ошибки, график отрисован?"]);
@@ -176,16 +176,13 @@ def finding_K(dp):
     K_ud=K_mass[ind]
     money=mass[ind]
     return money   
-def num_graph():
-    counter_graph=int(view.scale.get())
-    return counter_graph
 
 def critical():
     t0=1
     t_cl=30
     need_city=10000000
-    for i in range(num_graph()):
-        need_city=need_city * (10**(-i))-1000
+    for i in range(3):
+        need_city=need_city * (10**(-i))
         print("Need city: " + str(need_city))
         t0=1
         answer=[]
