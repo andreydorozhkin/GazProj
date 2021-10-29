a33808889b653b44647c2690075c8e280128b5f5
SolderingIronUser <wasiliysitnikov@gmail.com>
2021-10-29 06:07:35 +0300

    Update view.py

--- view.py
+++ view.py
@@ -9,12 +9,14 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 root = tk.Tk()
 root.geometry('1300x800')
 root["bg"]="white"
-frame1 = Frame(root); frame1.place(x=-15, y=-15, width=700, height=650)
-figure = plt.Figure(figsize=(5,5), facecolor='white', edgecolor="white")
+frame1 = Frame(root); frame1.place(x=-15, y=-15, width=700, height=650) #325 height
+figure = plt.Figure(figsize=(20,10), facecolor='white', edgecolor="white")
 canvas = FigureCanvasTkAgg(figure, frame1)
 canvas.get_tk_widget().place(x=10,y=10,width=700,height=650)
 ax = [figure.add_subplot(1,1,x+1)for x in range(1)]
-
+ax[0].set_title("Тут должен быть какой то текст", fontsize=14)
+ax[0].set_xlabel("t - время перехода с СПГ на ПГ", fontsize=14, color="#FF2300")
+ax[0].set_ylabel("L - Критическое расстояние", fontsize=14, color="#FF2300")
 
 #Визуальный метод определния материала
 def changeMaterials():
@@ -52,52 +54,44 @@ cost_gas = generate_field("Стоимость газа", X1, mass[0], X2, mass[0
 cost_natur_liquided_gas = generate_field("Стоимость СПГ", X1, mass[1], X2, mass[1])
 city_need_energy = generate_field("Потребность города в КВТ", X1, mass[2], X2, mass[2])
 cost_cistern = generate_field("Стоимость автомобильной цистерны", X1, mass[3], X2, mass[3])
-volume_cistern = generate_field("Объем автомобильной цистерны", X1, mass[4], X2, mass[4])
-number_cistern = generate_field("Колличество цистерн ", X1, mass[5], X2, mass[5])
-cost_tank = generate_field("Стоимость хранилища СПГ", X1, mass[6], X2, mass[6])
-volume_tank = generate_field("Объем хранилища СПГ", X1, mass[7], X2, mass[7])
-number_tank = generate_field("Колличество хранилищ )", X1, mass[8], X2, mass[8])
-cost_gasifiers = generate_field("Стоимость газификаторов ", X1, mass[9], X2, mass[9])
-cost_laying_high = generate_field("Стоимость прокладки газопровода высокого давления на километр", X1, mass[10], X2, mass[10])
-cost_laying_medium= generate_field("Стоимость прокладки газопровода среднего давления на километр", X1, mass[11], X2, mass[11])
-cost_GRPSH = generate_field("Стоимость ГРПШ", X1, mass[12], X2, mass[12])
-efficiency_GRPSH = generate_field("Производительность ГРПШ", X1, mass[13], X2, mass[13])
-cost_maintenance_GRPSH = generate_field("Стоимость обслуживания ГРПШ", X1, mass[14], X2, mass[14])
-cost_maintenance_gas_pipeline = generate_field("Стоимость обслуживания газопровода", X1, mass[15], X2, mass[15])
-factory_distance=generate_field("Расстояние от завода до города", X1, mass[16], X2, mass[16])
+#volume_cistern = generate_field("Объем автомобильной цистерны", X1, mass[4], X2, mass[4])
+number_cistern = generate_field("Колличество цистерн ", X1, mass[4], X2, mass[4])
+cost_tank = generate_field("Стоимость хранилища СПГ", X1, mass[5], X2, mass[5])
+volume_tank = generate_field("Объем хранилища СПГ", X1, mass[6], X2, mass[6])
+number_tank = generate_field("Колличество хранилищ ", X1, mass[7], X2, mass[7])
+cost_gasifiers = generate_field("Стоимость газификаторов ", X1, mass[8], X2, mass[8])
+# cost_laying_high = generate_field("Стоимость прокладки газопровода высокого давления на километр", X1, mass[10], X2, mass[10])
+# cost_laying_medium= generate_field("Стоимость прокладки газопровода среднего давления на километр", X1, mass[11], X2, mass[11])
+#cost_GRPSH = generate_field("Стоимость ГРПШ", X1, mass[12], X2, mass[12])
+#efficiency_GRPSH = generate_field("Производительность ГРПШ", X1, mass[13], X2, mass[13])
+#cost_maintenance_GRPSH = generate_field("Стоимость обслуживания ГРПШ", X1, mass[14], X2, mass[14])
+#cost_maintenance_gas_pipeline = generate_field("Стоимость обслуживания газопровода", X1, mass[15], X2, mass[15])
+factory_distance=generate_field("Расстояние от завода до города", X1, mass[9], X2, mass[9])
 gas_material = Label(root, text="Материал газопровода", background="white") 
-gas_material.place(x=X1,y=mass[17])
+gas_material.place(x=X1,y=mass[10])
 
 combo_exsample_gas_material = ttk.Combobox(root, 
                            values=["Сталь", "Полиэтилен"],
                            postcommand=changeMaterials, width=17)
-combo_exsample_gas_material.place(x=X2, y=mass[17])
+combo_exsample_gas_material.place(x=X2, y=mass[10])
 
 
 
 btplot1 = Button(root, text='Рассчитать',  # текст кнопки 
-                 background="#72D0F2",     # фоновый цвет кнопки
+                 background="#60B9CE",     # фоновый цвет кнопки
                  foreground="black",     # цвет текста
                  padx="20",                # отступ от границ до содержимого по горизонтали
                  pady="8",                 # отступ от границ до содержимого по вертикали
                  font='Tahoma 20', command= lambda: model.critical())
-btplot1.place(x=X1, y=mass[19], width=150, height=60)
+btplot1.place(x=X1, y=mass[12], width=150, height=60)
 
 btplot2 = Button(root, text='Очистить',  # текст кнопки 
-                 background="#72D0F2",     # фоновый цвет кнопки
+                 background="#60B9CE",     # фоновый цвет кнопки
                  foreground="black",     # цвет текста
                  padx="20",                # отступ от границ до содержимого по горизонтали
                  pady="8",                 # отступ от границ до содержимого по вертикали
                  font='Tahoma 20', command= lambda: model.clear())
-btplot2.place(x=X1, y=mass[22], width=150, height=60)
-
-btplot3 = Button(root, text='Critical',  # текст кнопки 
-                 background="#72D0F2",     # фоновый цвет кнопки
-                 foreground="black",     # цвет текста
-                 padx="20",                # отступ от границ до содержимого по горизонтали
-                 pady="8",                 # отступ от границ до содержимого по вертикали
-                 font='Tahoma 20', command = model.critical)
-btplot3.place(x=X1, y=mass[25], width=150, height=60)
+btplot2.place(x=X1, y=mass[15], width=150, height=60)
 
 
 root.mainloop()
