fe63b9ee59eb041a4970ffb4cac4ba42610bd414
Andrey Dorozhkin <andreydorozhkin.00@gmail.com>
2021-11-15 21:53:26 +0300

    lint

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from collections import Counter
-import view 
+import view
 import numpy as np
 import math
 import sys
@@ -7,102 +7,69 @@ import os
 import time
 
 
-
 def field_getter(field):
-    request=float(field.get())
+    request = float(field.get())
     return request
-#----------------------------------------------------------------------------
-# Все проверено логи можно не ставить
-# Не используй эту функциюю пока все не будет отлажено, вызывай critiacl по кнопке а не от сюда
-
-def exception_func():
-    array_entrys=[view.cost_gas,view.cost_natur_liquided_gas,
-                  view.city_need_energy_begin,
-                  view.city_need_energy_end,view.cost_cistern,
-                  view.volume_cistern,view.cost_tank,
-                  view.volume_tank,view.cost_gasifiers,
-                  view.factory_distance]
-    count=0
-    for i in array_entrys:
-        try:
-            val=float(i.get())
-            if val<=0:
-                view.message_info("Значение "+str(val)+" не должно быть отрицательным")
-                break
-            else:
-                count+=1
-            if count==len(array_entrys):
-                material=view.combo_exsample_gas_material.get()
-                if material=="Сталь" or material=="Полиэтилен":
-                    critical()
-                else:
-                    view.message_info("На данный момент возможный выбор материалов ограничен, "+
-                                       "пожалуйста, выберете материал из предалагемых вариантов")
-        except:
-            pass
-            view.message_error("Значение "+str(i.get())+" введено некорректно!\n"+
-            "Проверьте корректность введенных значений")
-            break           
-#-------------------------------------------------------------------------------
 
 
-
-def needing_city(): #тут тоже, но терпимо
-    begin=field_getter(view.city_need_energy_begin)
-    end=field_getter(view.city_need_energy_end)
+def needing_city():  # тут тоже, но терпимо
+    begin = field_getter(view.city_need_energy_begin)
+    end = field_getter(view.city_need_energy_end)
     mid = end / 2
-    need_city_list=[end, mid, begin]
+    need_city_list = [end, mid, begin]
     print("Список потребностей города:")
     print(need_city_list)
     return need_city_list
 
 
-def number_tank(need_city): #тут ошибка
-    tcm=20
-    insert_tank=field_getter(view.volume_tank)*8.83/1000
-    nt=[]
+def number_tank(need_city):  # тут ошибка
+    tcm = 20
+    insert_tank = field_getter(view.volume_tank) * 8.83 / 1000
+    nt = []
     print("Промежутки: ")
     while tcm <= 100:
-        qeq = (((need_city/1000)/12)*tcm)/40/insert_tank
+        qeq = (((need_city / 1000) / 12) * tcm) / 40 / insert_tank
         print(str(qeq))
         nt.append(round(qeq))
-        tcm+=20
+        tcm += 20
     return 1
 
+
 def number_cistern():
-    #view.number_cistern.config(state="normal")
-    #view.number_cistern.insert(0,"1")
+    # view.number_cistern.config(state="normal")
+    # view.number_cistern.insert(0,"1")
     return 1
-    
- 
+
 
 def clear():
     python = sys.executable
-    os.execl(python, python, * sys.argv)
+    os.execl(python, python, *sys.argv)
 
-def leg(param):                                           #А вот здесь оно блядь не работает
-    print("Param = "+str(param))
-    if param==1:
-        legend=(view.figure.legend(loc = "upper left"))
-    if param==0:
-        legend=(view.figure.legend(loc = "upper left"))
+
+def leg(param):  # А вот здесь оно блядь не работает
+    print("Param = " + str(param))
+    if param == 1:
+        legend = (view.figure.legend(loc="upper left"))
+    if param == 0:
+        legend = (view.figure.legend(loc="upper left"))
         legend.remove()
 
+
 def do_plot(need_city, points):
-    t0=[]
-    t=1
-    l0=points
-    while t!=30:
+    t0 = []
+    t = 1
+    l0 = points
+    while t != 30:
         t0.append(t)
-        t=t+1
-    x=t0
-    y=l0
-    #[view.ax[x].clear() for x in range(1)]
-    view.ax[0].plot(x,y,label="Q="+str(need_city/1000)+"МВт*ч/год")
+        t = t + 1
+    x = t0
+    y = l0
+    # [view.ax[x].clear() for x in range(1)]
+    view.ax[0].plot(x, y, label="Q=" + str(need_city / 1000) + "МВт*ч/год")
     leg(1)
     leg(0)
-    #leg=view.figure.legend(loc = "upper left") #ВОТ БЛЯТЬ ЛЕГЕНДА
-    #leg.remove() # ВОТ БЛЯДЬ РЕАЛИЗАЦИЯ ЕЁ УДАЛЕНИЯ
+    # leg=view.figure.legend(loc = "upper left") #ВОТ БЛЯТЬ ЛЕГЕНДА
+    # leg.remove() # ВОТ БЛЯДЬ РЕАЛИЗАЦИЯ ЕЁ УДАЛЕНИЯ
     view.canvas.draw()
 
 
@@ -110,189 +77,209 @@ def do_plot(need_city, points):
 def capital_costs_ksg(Q, cost_liquid_gaz):
     costs = 2 * Q * cost_liquid_gaz
     return costs
+
+
 # Эксплуатационные затраты на комплекс по сжижению газа
 def operation_costs_ksg(K_ksg):
-    N1 = 0.1*K_ksg
-    N2 = 0.05 * K_ksg  
+    N1 = 0.1 * K_ksg
+    N2 = 0.05 * K_ksg
     N3 = 0.04 * K_ksg
     N4 = 0.1 * K_ksg
-    N5 = 0.11*K_ksg
+    N5 = 0.11 * K_ksg
     N_ksg = N1 + N2 + N3 + N4 + N5
     return N_ksg
 
 
 # капитальные вложения в систему газоснабжения объекта СПГ
-def capital_costs_spg(K_ksg, K_cist, K_khsv, K_gazif): 
-    costs = K_ksg+K_cist+K_khsv+K_gazif
+def capital_costs_spg(K_ksg, K_cist, K_khsv, K_gazif):
+    costs = K_ksg + K_cist + K_khsv + K_gazif
     return costs
+
 
 # Капитальные затраты на цистерны
 def capital_costs_cist(number, cost):
-    return number*cost
+    return number * cost
+
 
 # Эксплуатационные затраты на обслуживание ГРП
 def Q_year(need_city):
-    q = need_city/12185.4
+    q = need_city / 12185.4
     return q
+
 
 # Эксплуатационны затраты на ШГРП
 def operating_cost_shgrp(K_shgrp):
-    return K_shgrp/10
+    return K_shgrp / 10
 
-#Капитальные затраты на газораспределительные шкафы
+
+# Капитальные затраты на газораспределительные шкафы
 def capital_cost_GRPSH(Q):
-    cap_GRPSH=(19.35906314*Q*1380)/1800  #Q_y-Это список
+    cap_GRPSH = (19.35906314 * Q * 1380) / 1800  # Q_y-Это список
     return cap_GRPSH
+
 
 # Мощность газификатора
 def power_gazif():
-    return 200/1380
+    return 200 / 1380
+
 
 # Капитальные затраты на газификатор
 def capital_costs_gazif(Q_year, power_gazif, cost_gazif):
-    capital=math.ceil(Q_year/power_gazif/365/24)
-    capital*=cost_gazif
+    capital = math.ceil(Q_year / power_gazif / 365 / 24)
+    capital *= cost_gazif
     return capital
+
 
 # Эксплуатационные затраты на газификатор
 def operating_costs_gazif(capital_costs):
     return capital_costs / 100
 
+
 # Капитальные затраты на хранилища
 def capital_costs_storage(num_storage, cost_storage):
     return num_storage * cost_storage
 
+
 # Эксплуатационные затраты на хранилища
 def operating_costs_storage(K_chsw):
-    return K_chsw*0.5/100
+    return K_chsw * 0.5 / 100
 
-#Экспулатауционные затраты на цистерны  #Новый пункт
+
+# Экспулатауционные затраты на цистерны  #Новый пункт
 def exp_cost_cist(number_cist):
-    exp=(number_cist/100)*5
+    exp = (number_cist / 100) * 5
     return exp
+
 
 # Эксплутационные расходы по доставке СПГ
 def operating_costs_spg(N_cist, N_schw, N_gazif, N_ksg, Q_year, a):
-    numerator = Q_year * a/0.9
-    costs = numerator + N_cist + N_ksg  + N_schw + N_gazif
+    numerator = Q_year * a / 0.9
+    costs = numerator + N_cist + N_ksg + N_schw + N_gazif
     return costs
+
 
 # Коэффциент дисконтирования (вместо t_cl может прийти t0)
 def discount_rate(t_cl, E):
-    numerator=(1+E)**t_cl-1
-    denominator=((1+E)**t_cl)*E
-    rate = numerator/denominator
+    numerator = (1 + E) ** t_cl - 1
+    denominator = ((1 + E) ** t_cl) * E
+    rate = numerator / denominator
     return rate
+
 
 # Остаточная ликвидационная стоиомость
 def liquidation_value(K_ksg, K_cist, K_khsv, K_gazif, t0, t_cl, K_hswd):
-
-    one_mltpr = (t_cl-t0)/t_cl*(K_ksg+K_cist+K_gazif+K_khsv-K_hswd)
+    one_mltpr = (t_cl - t0) / t_cl * (K_ksg + K_cist + K_gazif + K_khsv - K_hswd)
     return one_mltpr
 
+
 # Расчет критического радиуса
-def critical_distance(K_spg, Y_tcl,  N_spg, Y_t0, K_shgrp, L_spg, C_pg, Q_year, kpd, N_shgrp, K_ud, t_cl):
-    one_step=Y_tcl*N_spg
-    two_step=Y_t0*(N_spg+K_shgrp-L_spg)
-    three_step1=Y_tcl-Y_t0
-    three_step2=((C_pg*Q_year*1380)/kpd)+N_shgrp
-    three_step=three_step1*three_step2
-    numerator=one_step-two_step-three_step
-    four_step=(Y_tcl-Y_t0)
-    ext_step=(4*t_cl)
-    ext2_step=four_step/ext_step
-    five_step=ext2_step+Y_t0
-    denominator=K_ud*five_step
-    #print(numerator)
-    #print(denominator)
-    distance=numerator/denominator
+def critical_distance(K_spg, Y_tcl, N_spg, Y_t0, K_shgrp, L_spg, C_pg, Q_year, kpd, N_shgrp, K_ud, t_cl):
+    one_step = Y_tcl * N_spg
+    two_step = Y_t0 * (N_spg + K_shgrp - L_spg)
+    three_step1 = Y_tcl - Y_t0
+    three_step2 = ((C_pg * Q_year * 1380) / kpd) + N_shgrp
+    three_step = three_step1 * three_step2
+    numerator = one_step - two_step - three_step
+    four_step = (Y_tcl - Y_t0)
+    ext_step = (4 * t_cl)
+    ext2_step = four_step / ext_step
+    five_step = ext2_step + Y_t0
+    denominator = K_ud * five_step
+    # print(numerator)
+    # print(denominator)
+    distance = numerator / denominator
     return distance
 
+
 def diametr(Q0):
-    P_ud = 0.25/(1.1*field_getter(view.factory_distance)*1000) #view.factory_distance
+    P_ud = 0.25 / (1.1 * field_getter(view.factory_distance) * 1000)  # view.factory_distance
     p0 = 0.101325
-    A=0.101325/0.6*162*(pow(3.14,2))
-    material_steel=[0.022, 2, 5] 
-    material_polyethylene=[0.0446, 1.75, 4.75]  #A,m1,m2
-    B=material_steel[0]
-    m=material_steel[1]
-    m1=material_steel[2]
-    dp=(A*B*p0*(pow(Q0,m)))/P_ud
-    x=dp
-    y=m1
-    dp=pow(x,(1/y))
+    A = 0.101325 / 0.6 * 162 * (pow(3.14, 2))
+    material_steel = [0.022, 2, 5]
+    material_polyethylene = [0.0446, 1.75, 4.75]  # A,m1,m2
+    B = material_steel[0]
+    m = material_steel[1]
+    m1 = material_steel[2]
+    dp = (A * B * p0 * (pow(Q0, m))) / P_ud
+    x = dp
+    y = m1
+    dp = pow(x, (1 / y))
     return dp
 
 
-#Нахождение К-удельное
+# Нахождение К-удельное
 def finding_K(dp):
-    K_mass=[80,100,150,200,250,300] 
-    mass=[4864558, 5756494, 6110114, 7401794, 8427619, 10360698  ]
-    array_difference=[]
-    K_ud=float()
+    K_mass = [80, 100, 150, 200, 250, 300]
+    mass = [4864558, 5756494, 6110114, 7401794, 8427619, 10360698]
+    array_difference = []
+    K_ud = float()
     for i in K_mass:
-        if i>=dp:
-            array_difference=array_difference+[i-dp]
-        if i<=dp:
-            array_difference=array_difference+[dp-i]
+        if i >= dp:
+            array_difference = array_difference + [i - dp]
+        if i <= dp:
+            array_difference = array_difference + [dp - i]
     mylist = array_difference
-    doubles=[k for k,v in Counter(mylist).items() if v>1]
-    if len(doubles)==1:
+    doubles = [k for k, v in Counter(mylist).items() if v > 1]
+    if len(doubles) == 1:
         smallest_number = min(array_difference)
-        ind=array_difference.index(smallest_number)
-        K_ud=K_mass[ind+1]
-        money=mass[ind+1]
+        ind = array_difference.index(smallest_number)
+        K_ud = K_mass[ind + 1]
+        money = mass[ind + 1]
         return money
     smallest_number = min(array_difference)
-    ind=array_difference.index(smallest_number)
-    K_ud=K_mass[ind]
-    money=mass[ind]
-    return money   
+    ind = array_difference.index(smallest_number)
+    K_ud = K_mass[ind]
+    money = mass[ind]
+    return money
+
 
 def critical():
     clear_txt_entry()
-    str_tank=""
-    str_cistern=""
-    t0=1
-    t_cl=30
-    it=0
-    need_city=needing_city() # view.city_need_energy
+    str_tank = ""
+    str_cistern = ""
+    t0 = 1
+    t_cl = 30
+    it = 0
+    need_city = needing_city()  # view.city_need_energy
     for i in need_city:
-        need_city=i
-        t0=1
-        it+=1
-        answer=[]
-        count_tank=number_tank(need_city)
+        need_city = i
+        t0 = 1
+        it += 1
+        answer = []
+        count_tank = number_tank(need_city)
         count_cistern = number_cistern()
-        
-        str_tank+=str(count_tank)+"\t"
-        str_cistern+=str(count_cistern)+"\t"
-        
-        
-        #view.text_entry.insert(2.0,str(count_cistern))
-        while t0!=30:
-            #view.number_tank.config(state="normal")
-            #view.number_tank.insert(0,str(number_tank))
-            #view.number_tank.config(state="readonly")
-            K_chsw = capital_costs_storage(count_tank, field_getter(view.cost_tank)) #view.number_tank   view.cost_tank  
+
+        str_tank += str(count_tank) + "\t"
+        str_cistern += str(count_cistern) + "\t"
+
+        # view.text_entry.insert(2.0,str(count_cistern))
+        while t0 != 30:
+            # view.number_tank.config(state="normal")
+            # view.number_tank.insert(0,str(number_tank))
+            # view.number_tank.config(state="readonly")
+            K_chsw = capital_costs_storage(count_tank,
+                                           field_getter(view.cost_tank))  # view.number_tank   view.cost_tank  
             CityYear = Q_year(need_city)
-            K_gazif = capital_costs_gazif(CityYear, power_gazif(), field_getter(view.cost_gasifiers)) #  view.cost_gasifiers
-            a = field_getter(view.cost_natur_liquided_gas) # view.cost_natur_liquided_gas
+            K_gazif = capital_costs_gazif(CityYear, power_gazif(),
+                                          field_getter(view.cost_gasifiers))  # view.cost_gasifiers
+            a = field_getter(view.cost_natur_liquided_gas)  # view.cost_natur_liquided_gas
             K_ksg = capital_costs_ksg(CityYear, a)
-            #view.number_cistern.config(state="normal")
-            #view.number_cistern.insert(0, str(count_cistern))
-            #view.number_cistern.config(state="readonly")
-            K_cist = capital_costs_cist(count_cistern, field_getter(view.cost_cistern)) #  view.number_cistern   view.cost_cistern
+            # view.number_cistern.config(state="normal")
+            # view.number_cistern.insert(0, str(count_cistern))
+            # view.number_cistern.config(state="readonly")
+            K_cist = capital_costs_cist(count_cistern,
+                                        field_getter(view.cost_cistern))  # view.number_cistern   view.cost_cistern
             K_spg = capital_costs_spg(K_ksg, K_cist, K_chsw, K_gazif)
             Y_tcl = discount_rate(t_cl, 0.1)
             Y_t0 = discount_rate(t0, 0.1)
-            N_spg = operating_costs_spg(exp_cost_cist(K_cist), operating_costs_storage(K_chsw), operating_costs_gazif(K_gazif), 
-                                operation_costs_ksg(K_ksg), CityYear, a)
+            N_spg = operating_costs_spg(exp_cost_cist(K_cist), operating_costs_storage(K_chsw),
+                                        operating_costs_gazif(K_gazif),
+                                        operation_costs_ksg(K_ksg), CityYear, a)
             K_shgrp = capital_cost_GRPSH(CityYear)
             L_spg = liquidation_value(K_ksg, K_cist, K_chsw, K_gazif, t0, t_cl, 40000)
-            C_pg = field_getter(view.cost_gas)   #  view.cost_gas
+            C_pg = field_getter(view.cost_gas)  # view.cost_gas
             kpd = 0.9
-            N_shgrp = operating_cost_shgrp(K_shgrp)  
+            N_shgrp = operating_cost_shgrp(K_shgrp)
             K_ud = finding_K(diametr(CityYear))
             # print("=================")
             # print("Tcl:" + str(t_cl))
@@ -314,51 +301,50 @@ def critical():
             # print("Ишгрп: " + str(N_shgrp))
             # print("Куд: " + str(K_ud))
             # print("=================")
-            answer.append(critical_distance(K_spg, Y_tcl, N_spg, Y_t0, K_shgrp, L_spg, C_pg, CityYear, kpd, N_shgrp, K_ud, t_cl))
-            #view.message_info(["Request!", "Ответ: " + answer])
-            t0+=1 
+            answer.append(
+                critical_distance(K_spg, Y_tcl, N_spg, Y_t0, K_shgrp, L_spg, C_pg, CityYear, kpd, N_shgrp, K_ud, t_cl))
+            # view.message_info(["Request!", "Ответ: " + answer])
+            t0 += 1
         do_plot(need_city, answer)
     view.text_entry.configure(state="normal")
     view.text_entry.insert(1.24, str_tank)
     view.text_entry.insert(2.23, str_cistern)
     view.text_entry.configure(state='disabled')
-    
+
 
 def clear_txt_entry():
     view.text_entry.configure(state="normal")
     view.text_entry.delete(1.23, 1.33)
     view.text_entry.delete(2.22, 2.33)
-    view.text_entry.configure(state='disabled')    
+    view.text_entry.configure(state='disabled')
 
 
 def clear_entry():
-     view.cost_gas.delete(0, 'end')
-     view.cost_natur_liquided_gas.delete(0, 'end')
-     view.cost_cistern.delete(0, 'end')
-     view.volume_cistern.delete(0, 'end')
-     view.cost_tank.delete(0, 'end')
-     view.volume_tank.delete(0, 'end')
-     view.cost_gasifiers.delete(0, 'end')
-     view.factory_distance.delete(0, 'end')
-     view.combo_exsample_gas_material.delete(0,"end")
-     view.city_need_energy_begin.delete(0, "end")
-     view.city_need_energy_end.delete(0, "end")
+    view.cost_gas.delete(0, 'end')
+    view.cost_natur_liquided_gas.delete(0, 'end')
+    view.cost_cistern.delete(0, 'end')
+    view.volume_cistern.delete(0, 'end')
+    view.cost_tank.delete(0, 'end')
+    view.volume_tank.delete(0, 'end')
+    view.cost_gasifiers.delete(0, 'end')
+    view.factory_distance.delete(0, 'end')
+    view.combo_exsample_gas_material.delete(0, "end")
+    view.city_need_energy_begin.delete(0, "end")
+    view.city_need_energy_end.delete(0, "end")
 
-     #view.number_cistern.delete(0,"end")
+    # view.number_cistern.delete(0,"end")
+
 
 def test():
     # 1 Значение название поля Entry, 2 Запись поля
-    view.cost_gas.insert(0,"9.5")
-    view.cost_natur_liquided_gas.insert(0,"18268.68711")
-    view.cost_cistern.insert(0,"27768000")
-    view.volume_cistern.insert(0,"34700.1")
-    view.cost_tank.insert(0,"4049849.86")
-    view.volume_tank.insert(0,"72643.2")
-    view.cost_gasifiers.insert(0,"2554200")
-    view.factory_distance.insert(0,"10")
-    view.combo_exsample_gas_material.insert(0,"Сталь")
+    view.cost_gas.insert(0, "9.5")
+    view.cost_natur_liquided_gas.insert(0, "18268.68711")
+    view.cost_cistern.insert(0, "27768000")
+    view.volume_cistern.insert(0, "34700.1")
+    view.cost_tank.insert(0, "4049849.86")
+    view.volume_tank.insert(0, "72643.2")
+    view.cost_gasifiers.insert(0, "2554200")
+    view.factory_distance.insert(0, "10")
+    view.combo_exsample_gas_material.insert(0, "Сталь")
     view.city_need_energy_begin.insert(0, "100000")
     view.city_need_energy_end.insert(0, "10000000")
-
-    
-
--- view.py
+++ view.py
@@ -8,39 +8,48 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 root = tk.Tk()
 root.geometry('1300x650')
-root.resizable(width=False, height=False) 
+root.resizable(width=False, height=False)
 root.title("Main Screen")
-root["bg"]="white"
+root["bg"] = "white"
 
-frame1 = Frame(root); frame1.place(x=0, y=0, width=700, height=700) #325 height
-figure = plt.Figure(figsize=(5,5), facecolor='white', edgecolor="white")
+frame1 = Frame(root);
+frame1.place(x=0, y=0, width=700, height=700)  # 325 height
+figure = plt.Figure(figsize=(5, 5), facecolor='white', edgecolor="white")
 canvas = FigureCanvasTkAgg(figure, frame1)
-canvas.get_tk_widget().place(x=0,y=0,width=700,height=650)
-ax = [figure.add_subplot(1,1,x+1)for x in range(1)]
+canvas.get_tk_widget().place(x=0, y=0, width=700, height=650)
+ax = [figure.add_subplot(1, 1, x + 1) for x in range(1)]
 ax[0].set_xlabel("Время газификации опорного пункта \nсетевым природным газом t0 лет",
-                fontsize=12, color="black")
+                 fontsize=12, color="black")
 ax[0].set_ylabel("Удаленность потребителя от опорного пунка \nэнергоснабжения L, км",
-                fontsize=12, color="black")
+                 fontsize=12, color="black")
 
-#Визуальный метод определния материала
+
+# Визуальный метод определния материала
 def changeMaterials():
-   combo_exsample_gas_material["values"] = ["Сталь", "Полиэтилен"]
+    combo_exsample_gas_material["values"] = ["Сталь", "Полиэтилен"]
 
-#Вывод ошибок
+
+# Вывод ошибок
 def message_info(message):
-    messagebox.showinfo("Внимание!",message) 
+    messagebox.showinfo("Внимание!", message)
+
+
 def message_ask(message):
-    messagebox.askquestion("Внимание!",message)
+    messagebox.askquestion("Внимание!", message)
+
+
 def message_error(message):
     messagebox.showerror("Error!", message)
 
+
 def generate_coordinat(a, b, c):
-    coordinat=[]
-    while c!=a:
-        c+=1
-        coordinat=coordinat+[b]
-        b=b+25
+    coordinat = []
+    while c != a:
+        c += 1
+        coordinat = coordinat + [b]
+        b = b + 25
     return coordinat
+
 
 def generate_field(name_field, x_label, y_label, x_entry, y_entry):
     lab = Label(root, text=name_field, background="white")
@@ -49,74 +58,69 @@ def generate_field(name_field, x_label, y_label, x_entry, y_entry):
     text.place(x=x_entry, y=y_entry)
     return text
 
-mass=generate_coordinat(30, 70, 0)
-X1=775
-X2=1050
 
+mass = generate_coordinat(30, 70, 0)
+X1 = 775
+X2 = 1050
 
 cost_gas = generate_field("Стоимость газа", X1, mass[0], X2, mass[0])
 cost_natur_liquided_gas = generate_field("Стоимость СПГ", X1, mass[1], X2, mass[1])
 lbl = Label(root, text="Потребность города в КВт", background="white")
-lbl.place(x=X1,y=mass[2])
+lbl.place(x=X1, y=mass[2])
 city_need_energy_begin = generate_field("от", 1030, mass[2], X2, mass[2])
 city_need_energy_end = generate_field("до", 1030, mass[3], X2, mass[3])
 
 cost_cistern = generate_field("Стоимость автомобильной цистерны", X1, mass[5], X2, mass[5])
 volume_cistern = generate_field("Объем автомобильной цистерны", X1, mass[6], X2, mass[6])
-#number_cistern = generate_field("Колличество цистерн ", X1, mass[6], X2, mass[6])
-#number_cistern.config(state = "readonly")
+# number_cistern = generate_field("Колличество цистерн ", X1, mass[6], X2, mass[6])
+# number_cistern.config(state = "readonly")
 cost_tank = generate_field("Стоимость хранилища СПГ", X1, mass[8], X2, mass[8])
 volume_tank = generate_field("Объем хранилища СПГ", X1, mass[9], X2, mass[9])
-#number_tank = generate_field("Колличество хранилищ ", X1, mass[9], X2, mass[9])
-#number_tank.config(state = "readonly")
+# number_tank = generate_field("Колличество хранилищ ", X1, mass[9], X2, mass[9])
+# number_tank.config(state = "readonly")
 cost_gasifiers = generate_field("Стоимость газификаторов ", X1, mass[11], X2, mass[11])
 # cost_laying_high = generate_field("Стоимость прокладки газопровода высокого давления на километр", X1, mass[10], X2, mass[10])
 # cost_laying_medium= generate_field("Стоимость прокладки газопровода среднего давления на километр", X1, mass[11], X2, mass[11])
-#cost_GRPSH = generate_field("Стоимость ГРПШ", X1, mass[12], X2, mass[12])
-#efficiency_GRPSH = generate_field("Производительность ГРПШ", X1, mass[13], X2, mass[13])
-#cost_maintenance_GRPSH = generate_field("Стоимость обслуживания ГРПШ", X1, mass[14], X2, mass[14])
-#cost_maintenance_gas_pipeline = generate_field("Стоимость обслуживания газопровода", X1, mass[15], X2, mass[15])
-factory_distance=generate_field("Расстояние от завода до города", X1, mass[12], X2, mass[12])
-gas_material = Label(root, text="Материал газопровода", background="white") 
-gas_material.place(x=X1,y=mass[13])
+# cost_GRPSH = generate_field("Стоимость ГРПШ", X1, mass[12], X2, mass[12])
+# efficiency_GRPSH = generate_field("Производительность ГРПШ", X1, mass[13], X2, mass[13])
+# cost_maintenance_GRPSH = generate_field("Стоимость обслуживания ГРПШ", X1, mass[14], X2, mass[14])
+# cost_maintenance_gas_pipeline = generate_field("Стоимость обслуживания газопровода", X1, mass[15], X2, mass[15])
+factory_distance = generate_field("Расстояние от завода до города", X1, mass[12], X2, mass[12])
+gas_material = Label(root, text="Материал газопровода", background="white")
+gas_material.place(x=X1, y=mass[13])
 
-combo_exsample_gas_material = ttk.Combobox(root, 
-                           values=["Сталь", "Полиэтилен"],
-                           postcommand=changeMaterials, width=17)
+combo_exsample_gas_material = ttk.Combobox(root,
+                                           values=["Сталь", "Полиэтилен"],
+                                           postcommand=changeMaterials, width=17)
 combo_exsample_gas_material.place(x=X2, y=mass[13])
-text_entry=Text(root, height=8, width=34)
-text_entry.insert(1.0,"Колличество хранилищ = "+"\nКолличество цистерн = ")
-#text_entry.insert(2.0,"Колличество цистерн = ")
+text_entry = Text(root, height=8, width=34)
+text_entry.insert(1.0, "Колличество хранилищ = " + "\nКолличество цистерн = ")
+# text_entry.insert(2.0,"Колличество цистерн = ")
 text_entry.configure(state='disabled')
 text_entry.place(x=900, y=mass[16])
 
-
-
-
-
 btplot1 = Button(root, text='Рассчитать',  # текст кнопки 
-                 background="#60B9CE",     # фоновый цвет кнопки
-                 foreground="black",     # цвет текста
-                 padx="20",                # отступ от границ до содержимого по горизонтали
-                 pady="8",                 # отступ от границ до содержимого по вертикали
-                 font='Tahoma 14', command= lambda: model.exception_func())
-btplot1.place(x=X1, y=mass[16], width=150/1.5, height=60/1.5)
+                 background="#60B9CE",  # фоновый цвет кнопки
+                 foreground="black",  # цвет текста
+                 padx="20",  # отступ от границ до содержимого по горизонтали
+                 pady="8",  # отступ от границ до содержимого по вертикали
+                 font='Tahoma 14', command=lambda: model.critical())
+btplot1.place(x=X1, y=mass[16], width=150 / 1.5, height=60 / 1.5)
 
 btplot2 = Button(root, text='Очистить',  # текст кнопки 
-                 background="#60B9CE",     # фоновый цвет кнопки
-                 foreground="black",     # цвет текста
-                 padx="20",                # отступ от границ до содержимого по горизонтали
-                 pady="8",                 # отступ от границ до содержимого по вертикали
-                 font='Tahoma 14', command= lambda: model.clear())
-btplot2.place(x=X1, y=mass[18], width=150/1.5, height=60/1.5)
+                 background="#60B9CE",  # фоновый цвет кнопки
+                 foreground="black",  # цвет текста
+                 padx="20",  # отступ от границ до содержимого по горизонтали
+                 pady="8",  # отступ от границ до содержимого по вертикали
+                 font='Tahoma 14', command=lambda: model.clear())
+btplot2.place(x=X1, y=mass[18], width=150 / 1.5, height=60 / 1.5)
 
 btplot2 = Button(root, text='Тест',  # текст кнопки 
-                 background="#60B9CE",     # фоновый цвет кнопки
-                 foreground="black",     # цвет текста
-                 padx="20",                # отступ от границ до содержимого по горизонтали
-                 pady="8",                 # отступ от границ до содержимого по вертикали
-                 font='Tahoma 14', command= lambda: model.test())
-btplot2.place(x=X1, y=mass[20], width=150/1.5, height=60/1.5)
-
+                 background="#60B9CE",  # фоновый цвет кнопки
+                 foreground="black",  # цвет текста
+                 padx="20",  # отступ от границ до содержимого по горизонтали
+                 pady="8",  # отступ от границ до содержимого по вертикали
+                 font='Tahoma 14', command=lambda: model.test())
+btplot2.place(x=X1, y=mass[20], width=150 / 1.5, height=60 / 1.5)
 
 root.mainloop()
